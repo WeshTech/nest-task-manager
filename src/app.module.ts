32808feac0df2249f77078d928f55f51a9fa89c4
Engineer David <davidwaweru2500@gmail.com>
2026-01-13 09:33:23 +0300

    tasks

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), AuthModule, PrismaModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
